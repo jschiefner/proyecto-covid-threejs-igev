52e355a19cc3061abc910a08eda707c26501833e
Jonas Schiefner <jonas.schiefner@zweitag.de>
2022-03-21 00:29:12 +0100

    update update_database script, should now work properly

--- backend/update_database.js
+++ backend/update_database.js
@@ -25,7 +25,44 @@ file.forEach(element => {
 console.log("Total Objects: ", file.length)
 console.log("Distribution of present data:", counter)
 
+function firstDayOfWeek(year, week) {
+    // Jan 1 of 'year'
+    let date = new Date(year, 0, 1)
+    let offset = date.getTimezoneOffset()
 
+    // ISO: week 1 is the one with the year's first Thursday
+    // so nearest Thursday: current date + 4 - current day number
+    // Sunday is converted from 0 to 7
+    date.setDate(date.getDate() + 4 - (date.getDay() || 7))
+
+    // 7 days * (week - overlapping first week)
+    date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000
+        * (week + (year == date.getFullYear() ? -1 : 0 )))
+
+    // daylight savings fix
+    date.setTime(date.getTime()
+        + (date.getTimezoneOffset() - offset) * 60 * 1000)
+
+    // back to Monday (from Thursday)
+    date.setDate(date.getDate() - 3)
+
+    return date
+}
+
+function parseDate(dateString) {
+    let [year, week] = dateString.split('-')
+    year = parseInt(year)
+    week = parseInt(week)
+
+    if (year == NaN || week == NaN) {
+        throw new ArgumentError(`something wrong with this datestring: ${dateString}, year: ${year}, week: ${week}`)
+    }
+
+    return firstDayOfWeek(year, week)
+}
+
+
+// example from file
 // {
 //     country: 'Sweden',
 //     region_name: 'Norrbottens Lan',
@@ -37,17 +74,27 @@ console.log("Distribution of present data:", counter)
 //     source: 'Epidemic intelligence subnational data'
 // }
 
-let element = file[[file.length-1]];
+models.caseweek.findOne({order: [['date', 'DESC']]}).then((last) => {
+    const lastTime = last.date.getTime()
 
-const caseweek = models.caseweek.build({
-    country: element.country,
-    region: element.region_name,
-    nuts: element.nuts_code,
-    date: element.year_week,
-    incidence: element.rate_14_day_per_100k,
-    count: element.weekly_count,
-    population: element.population,
-    source: element.source,
+    let counter = 0;
+    file.forEach(element => {
+        const caseweek = models.caseweek.build({
+            country: element.country,
+            region: element.region_name,
+            nuts: element.nuts_code,
+            date: parseDate(element.year_week),
+            incidence: element.rate_14_day_per_100k,
+            count: element.weekly_count,
+            population: element.population,
+            source: element.source,
+        })
+
+
+        if (caseweek.date.getTime() > lastTime) {
+            caseweek.save()
+            counter++
+        }
+    })
+    console.log(`Saved ${counter} new entries`)
 })
-
-caseweek.save()
