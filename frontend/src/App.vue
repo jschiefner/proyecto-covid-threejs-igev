86ba3dec9759e0e4cbbe95f8b51381feae7382c6
Jonas Schiefner <jonas.schiefner@zweitag.de>
2022-03-21 23:10:52 +0100

    clean up remove unused files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import Car from './components/Car.vue'
 import Globe from './components/Globe.vue'
 </script>
 
