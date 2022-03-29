b88fef2e6e0ee5c88c73458a85ce00665251e6b3
Jonas Schiefner <jonas.schiefner@zweitag.de>
2022-03-29 20:33:41 +0200

    start work on sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,10 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import Globe from './components/Globe.vue'
+import Sidebar from './components/Sidebar.vue'
 </script>
 
 <template>
-  <Globe />
+  <div>
+    <Sidebar />
+    <Globe />
+  </div>
 </template>
 
 <style>
---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,44 @@
+<script setup>
+import "bulma/css/bulma.css";
+</script>
+
+<template>
+  <div id="menucontainer" class="container">
+    <h4 class="title is-4">¿A Donde quieres Viajar?</h4>
+    <div class="field has-addons">
+      <p class="control has-icons-left">
+        <input class="input" placeholder="Ciudad, Region o País" />
+        <span class="icon is-small is-left">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </span>
+      </p>
+      <div class="control">
+        <a class="button is-info">Buscar</a>
+      </div>
+    </div>
+    <hr />
+
+  </div>
+</template>
+
+<style scoped>
+div#menucontainer {
+  position: absolute;
+  /* top: 0px;
+    left: 0px; */
+  height: 100%;
+  width: 33%;
+  background: #ffffff11;
+  z-index: 1;
+  color: white;
+  text-align: center;
+  padding: 1rem;
+}
+h4 {
+  color: white;
+}
+
+.field {
+    justify-content: center;
+}
+</style>
