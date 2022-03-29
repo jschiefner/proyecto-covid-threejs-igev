2a76b6e3f6b7e4d812260b67fba84a5f3e8f3fac
Jonas Schiefner <jonas.schiefner@zweitag.de>
2022-03-30 00:04:47 +0200

    use vue-datepicker-next

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,6 +1,9 @@
 <script setup>
-import { computed } from "@vue/reactivity";
+import { computed, ref } from "@vue/reactivity";
 import "bulma/css/bulma.css";
+import { watch } from "vue";
+import DatePicker from 'vue-datepicker-next';
+import 'vue-datepicker-next/index.css';
 
 const props = defineProps({
   covidData: Array,
@@ -15,6 +18,12 @@ const selectedRegion = computed({
     return props.covidData.find((element) => element.nuts == props.selectedNutsCode);
   },
 });
+
+const selectedDate = ref(new Date());
+const setSelectedDate = function(date) {
+  const diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+  selectedDate.value = new Date(date.setDate(diff))
+}
 
 const emit = defineEmits(["search"]);
 </script>
@@ -33,6 +42,8 @@ const emit = defineEmits(["search"]);
         <a class="button is-info" @click="this.$emit('search')">Buscar</a>
       </div>
     </div>
+    <hr />
+    <date-picker :value="selectedDate" @change="setSelectedDate" type="week" format="DD/MM/YYYY" :clearable="false" :lang="{formatLocale: {firstDayOfWeek: 1}}"></date-picker>
     <hr />
     <h1>selected region: {{selectedRegion == null ? "none" : selectedRegion.region}}</h1>
   </div>
